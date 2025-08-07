8834cc80f8a6d11fd7e1ff1689584175f0e486e1
Dylan Broens <dylan.broens@student.gildeopleidingen.nl>
2025-08-08 00:56:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',    // <-- listen on all IPs
+        port: 5173,         // <-- default Vite port
+        strictPort: true,
         cors: true,
     },
 });
